5e2a626f54810387d11f621a0fa16a94f72e1e86
Topgyal Gurung <topgyaltsering3@gmail.com>
2019-06-01 11:17:48 -0400

    work on add column

--- assets/pixel.js
+++ assets/pixel.js
@@ -10,20 +10,29 @@ function delRow(){
     let row=table.deleteRow(0);
     let cell=row.deleteCell(0);
 }
-function addColumn(){
-    let table=document.getElementById("table");
-    let column=table.addColumn(0);
-    let cell=column.deleteCell(0)
-}
+// function addColumn(){
+//     let colNum=0;
+//     let rows=document.querySelector('#'+'table'+'tr');
+//     for(let i=0;i<rows.clientHeight;i++){
+//         let c=document.createElement('td');
+//         rows[i].appendChild(c);
+//     }
+// colNum++
+//     // let table=document.getElementById("table");
+//     // let column=table.addColumn(0);
+//     // let cell=column.deleteCell(0)
+// }
 function delColumn(){
     let table=document.getElementById("table");
     let column=table.deleteColumn(0);
-    let cell=column.deleteCell(0)
+    let cell=column.deleteCell(0);
 }
 
 function changeColor(){
     let select=document.getElementById("choice").value;
-    document.getElementById("box").style.backgroundColor=select;
+    var x= document.getElementsByTagName("TR");
+
+   
 }
 
 //fill all uncolored cells with the currently selected color
@@ -39,7 +48,8 @@ function fillEmpty(){
 }
 
 function clearAll(){
-
+    let select=document.getElementById("white").value;
+    document.getElementById("tbody").style.background=select;
 }
 
 
