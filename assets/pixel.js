1d1effd05b0397346d9ce473c083fd26e036c754
Topgyal Gurung <topgyaltsering3@gmail.com>
2019-06-02 22:42:53 -0400

    modified styles

--- assets/pixel.js
+++ assets/pixel.js
@@ -7,12 +7,6 @@ function addRow(){
     let table= document.getElementById("table");
     let row=table.insertRow(0);
     let cell=row.insertCell(0);
-    // let r=document.createElement('tr');
-    // for(let i=0;i<colNum;i++){
-    //     let a=document.createElement('td');
-    //     r.appendChild(c);
-    // }
-    // document.getElementById('table').appendChild(r);
   
 }
 function delRow(){
@@ -20,51 +14,26 @@ function delRow(){
     let row=table.deleteRow(0);
     let cell=row.deleteCell(0);
 }
-// function addColumn(){
-//     // let rows=document.querySelectorAll('#'+'table'+'tr');
-//    let table=document.getElementById("tr");
-//     let column=table.addColumn(0);
-//     let cell=column.addCell(0)
-// }
 function addColumn () {
     if(colNum==0){
         addRow();
     } 
-    //addRow();
+
 	let rows = document.querySelectorAll('#' + 'table' +' tr');
 	for (let i = 0; i <rows.length; i++) {
 		let c = document.createElement('td');
-		//makeClickable(c);
 		rows[i].appendChild(c);
 	}
 	colNum++;
-
 }
 function delColumn(){
-    // if(colNum<=0){
-    //     return;
-    // }
-    // let rows=document.querySelectorAll('#'+'table'+'tr');
-    // for(let i=0;i<rows.length;i++){
-    //     rows[i].removeChild(rows[i].lastElementChild);
-    // }
-    // colNum--;
-
+ 
     let table=document.getElementById("table");
     let column=table.deleteColumn(0);
     let cell=column.deleteCell(0);
 }
 
-function changeColor(){
-    let select=document.getElementById("choice").value;
-    var x= document.getElementsByTagName("TR");
-    
-   
-}
-
-//fill all uncolored cells with the currently selected color
 function fillAll(){
-   // let fillColor=document.getElementById("choice").value;
    let select=document.getElementById("choice").value;
    document.getElementById("tbody").style.background=select;
 
@@ -77,19 +46,6 @@ function fillEmpty(){
 function clearAll(){
     let select=document.getElementById("white").value;
     document.getElementById("tbody").style.background=select;
-}
-
-let makeClickable=function(e){
-    e.props={
-        cliked:false
-    };
-    e.onclick=function(){
-        if(e.props.clicked==false){
-            e.style.backgroundColor=choice;
-        }else{
-            e.style.backgroundColor="white";
-        }
-    }
 }
 
 
